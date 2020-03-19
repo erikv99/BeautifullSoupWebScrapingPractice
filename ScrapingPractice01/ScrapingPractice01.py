542e121d995aec90fe7b12890da72249897154fe
Erik Veenstra <zerikveenstraz@gmail.com>
2020-03-19 12:46:17 +0100

    started

--- ScrapingPractice01/ScrapingPractice01.py
+++ ScrapingPractice01/ScrapingPractice01.py
@@ -1,7 +1,14 @@
-# Will try to scrape a site using beautifull soup and request a
+# Will try to scrape a site using beautifull soup and request 
+from bs4 import BeautifulSoup
+import requests
+
 def main():
 
-    
+    # getting the site we want to scrape in a "response object", then using the text() to get the source code.
+    source = requests.get("http://quotes.toscrape.com/").text
+    soup = BeautifulSoup(source, "lxml")
 
+    quotes = soup.find(class_="row").find(class_="col-md-8")
+    print(quotes.prettify())
 if (__name__ == "__main__"):
     main()
